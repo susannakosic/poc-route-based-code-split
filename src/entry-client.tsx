9ff70638444f620623048ebc54446fe63900e6f0
Susanna Kosic <susanna.kosic@gmail.com>
2022-12-02 07:11:43 +0000

    poc: router setup

---
+++ src/entry-client.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <Router />
+  </BrowserRouter>
+);
